2996d8ab4ee8b741a866d5f03685adb702ca1b1f
Mike Levin <miklevin@gmail.com>
2024-10-01 17:16:49 -0400

    Common packages back in

--- flake.nix
+++ flake.nix
@@ -14,13 +14,28 @@
         isLinux = pkgs.stdenv.isLinux;
         osString = if isDarwin then "macOS" else if isLinux then "Linux" else "Unknown OS";
 
+        # Define common packages used across all platforms
+        commonPackages = with pkgs; [
+          python311
+          python311.pkgs.pip
+          python311.pkgs.virtualenv
+          cmake
+          ninja
+          gcc
+          git
+          zlib
+          stdenv.cc.cc.lib
+          figlet
+          tmux
+        ];
+
         reportOS = pkgs.writeShellScriptBin "report-os" ''
           echo "Hello from ${osString}!"
           echo "Nix-detected system: ${system}"
         '';
 
         linuxDevShell = pkgs.mkShell {
-          buildInputs = [ reportOS ];
+          buildInputs = commonPackages ++ [ reportOS ];  # Added commonPackages
           shellHook = ''
             ${reportOS}/bin/report-os
             echo "This is a Linux-specific message."
@@ -28,7 +43,7 @@
         };
 
         darwinDevShell = pkgs.mkShell {
-          buildInputs = [ reportOS ];
+          buildInputs = commonPackages ++ [ reportOS ];  # Added commonPackages
           shellHook = ''
             ${reportOS}/bin/report-os
             echo "This is a macOS-specific message."
